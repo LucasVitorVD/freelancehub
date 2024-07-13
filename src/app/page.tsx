1fc6996dc959e86cc9d7114e85b6410181244cbc
LucasVitorVD <lucaskirito85@gmail.com>
2024-07-12 21:22:26 -0300

    refact: deleting partnersSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Hero from "@/components/Hero";
-import PartnersSection from "@/components/partners-section/PartnersSection";
+import PartnersCarousel from "@/components/PartnersCarousel";
 
 export default function HomePage() {
   return (
     <div>
       <Hero />
-      <PartnersSection />
+      <section className="space-y-6 pt-10">
+        <h2 className="text-2xl font-bold text-center md:text-4xl">
+          Nossos parceiros
+        </h2>
+        <PartnersCarousel />
+      </section>
     </div>
   );
 }
