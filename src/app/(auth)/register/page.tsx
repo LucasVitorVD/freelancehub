7eadbe3a3d509e530306760b58fdad777fbdee3b
LucasVitorVD <lucaskirito85@gmail.com>
2024-07-31 18:46:28 -0300

    feat: adding responsiveness and some animations to the register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Card,
@@ -8,15 +10,31 @@ import {
 } from "@/components/ui/card";
 import RegisterForm from "@/components/forms/RegisterForm";
 import { loginFormSchema, signInFormSchema } from "@/schemas/register-schema";
+import { Player } from "@lottiefiles/react-lottie-player";
+import welcomeAnimation from "@/assets/animations/welcome-animation.json"
 
 export default function RegisterPage() {
   return (
     <main className="flex flex-col items-center justify-center flex-1">
-      <section className="grid grid-cols-2 w-[900px] h-[800px] shadow-xl">
-        <div className="bg-blue-500 rounded-s-md">
-          <h1>Image</h1>
+      <section className="grid grid-cols-1 shadow-xl md:grid-cols-2 md:h-screen lg:p-4 lg:w-3/4 lg:h-[800px]">
+        <div className="flex flex-col items-center justify-center bg-primary pt-8 md:p-4 lg:rounded-s-md">
+          <div className="text-center space-y-2 text-white">
+            <h1 className="text-3xl font-bold">Seja bem-vindo!</h1>
+            <p>
+              Estamos entusiasmados por você se juntar à nossa comunidade. Faça
+              login ou crie uma conta para começar.
+            </p>
+          </div>
+
+          <Player
+            src={welcomeAnimation}
+            className="player"
+            loop
+            autoplay
+            style={{ height: '90%', width: '90%' }}
+          />
         </div>
-        <div className="rounded-e-md px-4">
+        <div className="rounded-e-md px-4 pt-4 lg:pt-0">
           <Tabs defaultValue="login" className="w-full">
             <TabsList className="grid grid-cols-2">
               <TabsTrigger value="login">Login</TabsTrigger>
